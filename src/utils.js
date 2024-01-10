305a59c20998d4ebcf52ce7e5380a0416f8da0b0
Hayden Roney <roneyhayden@gmail.com>
2024-01-10 11:17:34 -0600

    lab done

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,9 @@
-// Your code here
+export function pointsForWord(word) {
+    let points = 0;
+    word.match(/\d+/) ? word = word.replace(/\d+/g, '') : null
+    for (const char of word) {
+        console.log("char is: ", char, " and it's a type of ", typeof char)
+        points += /[aeiou]/i.test(char) ? 1 : 2;
+    }
+    return points;
+}
